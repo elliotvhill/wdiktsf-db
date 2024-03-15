adaf9ce4ca53af893e1591d25c2d2518ab2a0e73
Elliot <ellthrowaway2@icloud.com>
2024-03-15 17:25:42 -0400

    created artist and song models

--- wdiktsf_project/wdiktsf/models.py
+++ wdiktsf_project/wdiktsf/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+    
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100)
+    albums = models.TextField()
+
+    def __str__(self):
+        return self.title
